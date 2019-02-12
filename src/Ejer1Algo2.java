2b302b4d9fa1922b170498bf9a8896ec002765f2
luismsdb <100316410@alumnos.uc3m.es>
2019-02-13 00:23:10 +0100

    Add files via upload

--- src/Ejer1Algo2.java
+++ src/Ejer1Algo2.java
@@ -21,7 +21,7 @@ public class Ejer1Algo2 {
 		BigInteger diferencia;							//Diferencia entre numero1 y el producto de la iteracción actual
 		BigInteger diferenciaAnterior=0;				//Diferencia entre numero1 y el producto de la iteracción previa a la actual
 
-		for(int i= 	intValue(numero2)/2; i>0; i++){			//Número máximo de posibles iteraciones = la mitad del menor
+		for(int i=0; numero2.intValue()/2; i>0; i++){			//Número máximo de posibles iteraciones = la mitad del menor
 				numero=numero1.divide(numero2);	//Dividie y trunca el valor entero del resultado
 				producto=numero2.multiply(numero);
 				diferencia=numero1.subtract(producto);	//Diferencia entre numero1 y el producto de numero2 por el entero de mayor
