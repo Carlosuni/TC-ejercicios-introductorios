d711eea3840b2e07ffe7e3d85df5ec31c0c99ca0
CarlosUni <100074907@alumnos.uc3m.es>
2019-02-13 12:29:07 +0100

    Algoritmo 2 (Euclides) acabado.

--- src/Ejer1Algo2.java
+++ src/Ejer1Algo2.java
@@ -1,43 +1,44 @@
-import java.math.BigInteger
+import java.math.BigInteger;
 
 public class Ejer1Algo2 {
 	
-		public BigInteger euclides(BigInteger numero1, BigInteger numero2){
+	public static BigInteger euclides(BigInteger numero1, BigInteger numero2){
 		
 		if((numero2.compareTo(numero1) > 0)){			//Ordena los valores (numero1 mayor, numero 2 menor)
 			BigInteger auxiliar=numero1;
 			numero1=numero2;
 			numero2=auxiliar;
 		}
-		
-		BigInteger cero=0;
-		
-		if((numero2.compareTo(cero) != 0) || (numero1.compareTo(cero) != 0)){	//Si cualquiera de los números es 0, su mcd es 0
+			
+		BigInteger cero = new BigInteger("0");
+			
+		if((numero2.compareTo(cero) == 0) || (numero1.compareTo(cero) == 0)){	//Si cualquiera de los números es 0, su mcd es 0
 			return cero;
 		}
-		
-		BigInteger numero;								//Entero de mayor valor por el que se multiplica a numero2 cuyo resultado es menor que numero1
+			
+		BigInteger cociente;								//Entero de mayor valor por el que se multiplica a numero2 cuyo resultado es menor que numero1
 		BigInteger producto;							
-		BigInteger diferencia;							//Diferencia entre numero1 y el producto de la iteracción actual
-		BigInteger diferenciaAnterior=0;				//Diferencia entre numero1 y el producto de la iteracción previa a la actual
-
-		for(int i=0; numero2.intValue()/2; i>0; i++){			//Número máximo de posibles iteraciones = la mitad del menor
-				numero=numero1.divide(numero2);	//Dividie y trunca el valor entero del resultado
-				producto=numero2.multiply(numero);
-				diferencia=numero1.subtract(producto);	//Diferencia entre numero1 y el producto de numero2 por el entero de mayor
-
-				if(diferencia.equals(cero)){ 
-					if(diferenciaAnterior.equals(cero)){ 
-						return numero2;	//Si diferenciaAnterior es 0, la primera iteración nos da el mcd que es el propio valor de numero2
-					} else {
-						return diferenciaAnterior;
-					}
+		BigInteger resto;							//Diferencia entre numero1 y el producto de la iteracción actual
+		BigInteger restoAnterior = new BigInteger ("0");				//Diferencia entre numero1 y el producto de la iteracción previa a la actual
+	
+		for(int i = 0; i < (numero2.intValue() / 2); ++i){			//Número máximo de posibles iteraciones = la mitad del menor
+			cociente = numero1.divide(numero2);	//Dividie y trunca el valor entero del resultado
+			producto = numero2.multiply(cociente);
+			resto = numero1.subtract(producto);	//Diferencia entre numero1 y el producto de numero2 por el entero de mayor
+	
+			if(resto.equals(cero)){ 
+				if(restoAnterior.equals(cero)){ 
+					return numero2;		//Si diferenciaAnterior es 0, la primera iteración nos da el mcd que es el propio valor de numero2
 				} else {
-					numero1=numero2;
-					numero2=diferencia;		
+					return restoAnterior;
 				}
+			} else {
+				numero1 = numero2;
+				numero2 = resto;		
+			}
 		}
 		
+		return restoAnterior;
 	}
-
+	
 }
